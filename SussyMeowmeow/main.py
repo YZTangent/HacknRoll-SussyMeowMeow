447f9caccc9e5d344f4554654c65dd95a025390f
zrei <88964793+zrei@users.noreply.github.com>
2023-01-14 16:18:06 +0800

    Make all gifs play in full

--- SussyMeowmeow/main.py
+++ SussyMeowmeow/main.py
@@ -2,7 +2,7 @@
 import random
 import tkinter as tk
 
-x = 1400
+x = 0
 cycle = 0 #cycle is the index -> which frame we want to currently play
 check = 0
 idle = [1, 2, 3, 4, 5] # idle numbers
@@ -23,28 +23,28 @@ def event(cycle, check, event_number, x):
     #print(idle)
     if event_number in idle:
         check = 0
-        print('idle')
+        #print('idle')
         window.after(400, update, cycle, check, event_number, x)  # no. 1,2,3 = idle
     elif event_number in jump_Up:
         check = 1
-        print('jump up')
-        window.after(100, update, cycle, check, event_number, x)  # no. 4,5 = jump up
+        #print('jump up')
+        window.after(400, update, cycle, check, event_number, x)  # no. 4,5 = jump up
     elif event_number in jump_Right:
         check = 2
-        print('jump right')
-        window.after(100, update, cycle, check, event_number, x)  # no. 6,7 = jump right
+        #print('jump right')
+        window.after(400, update, cycle, check, event_number, x)  # no. 6,7 = jump right
     elif event_number in jump_Left:
         check = 3
-        print('jump left')
-        window.after(100, update, cycle, check, event_number, x)  # no 8,9 = jump left
+        #print('jump left')
+        window.after(400, update, cycle, check, event_number, x)  # no 8,9 = jump left
     elif event_number in roll_Left:
         check = 4
-        print('roll left')
-        window.after(100, update, cycle, check, event_number, x)  # no. 12,13 = roll left
+        #print('roll left')
+        window.after(400, update, cycle, check, event_number, x)  # no. 12,13 = roll left
     elif event_number in roll_Right:
         check = 5
-        print('roll right')
-        window.after(100, update, cycle, check, event_number, x)  # no. 14,15 = roll right
+        #print('roll right')
+        window.after(400, update, cycle, check, event_number, x)  # no. 14,15 = roll right
 
 
 # making gif work
@@ -52,6 +52,7 @@ def gif_work(cycle, frames, event_number, first_num, last_num):
     # cycle is the index, frames is the array, event_number is. what event
     # first_num and last_num is just for random calculation
     #print(cycle, frames, event_number, first_num, last_num)
+    debug_event(event_number)
     if cycle < len(frames) - 1:
         cycle += 1
     else:
@@ -70,37 +71,37 @@ def update(cycle, check, event_number, x):
     if check == 0:
         #print("Update check is 0")
         frame = idle_Frames[cycle]
-        cycle, event_number = gif_work(cycle, idle, event_number, 1, 9)
+        cycle, event_number = gif_work(cycle, idle_Frames, event_number, 1, 15)
     # jump up
     elif check == 1:
         #print("Update check is 1")
         frame = jump_Up_Frames[cycle]
-        cycle, event_number = gif_work(cycle, jump_Up, event_number, 1, 15)
+        cycle, event_number = gif_work(cycle, jump_Up_Frames, event_number, 1, 15)
         #print(cycle, event_number)
     # jump right
     elif check == 2:
         #print("Update check is 2")
         frame = jump_Right_Frames[cycle]
-        cycle, event_number = gif_work(cycle, jump_Right, event_number, 1, 15)
+        cycle, event_number = gif_work(cycle, jump_Right_Frames, event_number, 1, 15)
     # jump left
     elif check == 3:
         #print("Update check is")
         frame = jump_Left_Frames[cycle]
-        cycle, event_number = gif_work(cycle, jump_Left, event_number, 1, 15)
+        cycle, event_number = gif_work(cycle, jump_Left_Frames, event_number, 1, 15)
     # roll left
     elif check == 4:
         frame = roll_Left_Frames[cycle]
-        cycle, event_number = gif_work(cycle, roll_Left, event_number, 1,15)
-        x -= -3
+        cycle, event_number = gif_work(cycle, roll_Left_Frames, event_number, 1,15)
+        x = -3
     # roll right
     elif check == 5:
         frame = roll_Right_Frames[cycle]
-        cycle, event_number = gif_work(cycle, roll_Right, event_number, 1, 15)
-        x -= -3
+        cycle, event_number = gif_work(cycle, roll_Right_Frames, event_number, 1, 15)
+        x = 3
     # 480x480 is the window size
     # str(x) + 1050 is the position of the window
     # we only updates x on roll left or roll right because it changes position
-    window.geometry('480x480')#+') + str(x) + '+1050') # this makes the window go off-screen for me
+    window.geometry('480x480+' + str(480 + x) + '+200')#+ #str(x) + '+1050') # this makes the window go off-screen for me
     # 480 is sprite size, 1050 is supposed to be screen res
     #print("PROCEEDING")
     label.configure(image=frame)
@@ -109,6 +110,19 @@ def update(cycle, check, event_number, x):
     #print(cycle, check, event_number, x)
     window.after(1, event, cycle, check, event_number, x)
 
+def debug_event(event_number):
+    if event_number in idle:
+        print('idle')
+    elif event_number in jump_Up:
+        print('Jump up')
+    elif event_number in jump_Left:
+        print('Jump left')
+    elif event_number in jump_Right:
+        print('Jump right')
+    elif event_number in roll_Left:
+        print('Roll left')
+    elif event_number in roll_Right:
+        print('Roll right')
 
 window = tk.Tk()
 # call buddy's action gif
