06029bb6898a2892e176d7de4abea7da07fccd83
zrei <88964793+zrei@users.noreply.github.com>
2023-01-14 17:30:10 +0800

    Use current window locations for placement

--- SussyMeowmeow/main.py
+++ SussyMeowmeow/main.py
@@ -2,10 +2,9 @@
 import random
 import tkinter as tk
 
-x = 480 # I think this is screen dependent :') also we need to ensure it never rolls off the screen
-y = 200 # this is also screen dependent
+dimensions = (480, 480)
 cycle = 0 #cycle is the index -> which frame we want to currently play
-check = 0
+check = 0 # first animation is idle
 idle = [1, 2, 3, 4, 5] # idle numbers
 jump_Up = [6, 7]
 jump_Right = [8, 9]
@@ -18,41 +17,37 @@ impath = './Cat GIFs/'
 
 #window.after is after y ms, do the function action -> can provide parameters as further arguments
 # transfer random no. to event
-def event(cycle, check, event_number, x, y):
-    #print("!!!")
-    #print(cycle, check, event_number, x)
-    #print(idle)
+def event(cycle, check, event_number):
     if event_number in idle:
         check = 0
         #print('idle')
-        window.after(150, update, cycle, check, event_number, x, y)  # no. 1,2,3 = idle
+        window.after(150, update, cycle, check, event_number)  # no. 1,2,3 = idle
     elif event_number in jump_Up:
         check = 1
         #print('jump up')
-        window.after(150, update, cycle, check, event_number, x, y)  # no. 4,5 = jump up
+        window.after(150, update, cycle, check, event_number)  # no. 4,5 = jump up
     elif event_number in jump_Right:
         check = 2
         #print('jump right')
-        window.after(150, update, cycle, check, event_number, x, y)  # no. 6,7 = jump right
+        window.after(150, update, cycle, check, event_number)  # no. 6,7 = jump right
     elif event_number in jump_Left:
         check = 3
         #print('jump left')
-        window.after(150, update, cycle, check, event_number, x, y)  # no 8,9 = jump left
+        window.after(150, update, cycle, check, event_number)  # no 8,9 = jump left
     elif event_number in roll_Left:
         check = 4
         #print('roll left')
-        window.after(150, update, cycle, check, event_number, x, y)  # no. 12,13 = roll left
+        window.after(150, update, cycle, check, event_number)  # no. 12,13 = roll left
     elif event_number in roll_Right:
         check = 5
         #print('roll right')
-        window.after(100, update, cycle, check, event_number, x, y)  # no. 14,15 = roll right
+        window.after(150, update, cycle, check, event_number)  # no. 14,15 = roll right
 
 
 # making gif work
 def gif_work(cycle, frames, event_number, first_num, last_num):
     # cycle is the index, frames is the array, event_number is. what event
     # first_num and last_num is just for random calculation
-    #print(cycle, frames, event_number, first_num, last_num)
     #debug_event(event_number)
     if cycle < len(frames) - 1:
         cycle += 1
@@ -62,75 +57,66 @@ def gif_work(cycle, frames, event_number, first_num, last_num):
     return cycle, event_number
 
 
-def update(cycle, check, event_number, x, y):
-    #print("In update!")
+def update(cycle, check, event_number):
     # cycle is frame number
     # check is which action 
     # event_number is which event
     # x is the coordinate x
+
+    x = 0
+    y = 0
+
     # idle
     if check == 0:
-        #print("Update check is 0")
         frame = idle_Frames[cycle]
         cycle, event_number = gif_work(cycle, idle_Frames, event_number, 1, 15)
     # jump up
     elif check == 1:
-        #print("Update check is 1")
         frame = jump_Up_Frames[cycle]
         cycle, event_number = gif_work(cycle, jump_Up_Frames, event_number, 1, 15)
-        #print(cycle, event_number)
         if cycle < len(jump_Up_Frames) / 2:
-            print("Up")
-            y -= 2
+            y = -2
         else:
-            print("Down")
-            y += 2
+            y = 2
     # jump right
     elif check == 2:
-        #print("Update check is 2")
         frame = jump_Right_Frames[cycle]
         cycle, event_number = gif_work(cycle, jump_Right_Frames, event_number, 1, 15)
-        x += 2
+        x = 2
         if cycle < len(jump_Right_Frames) / 2:
-            y -= 2
-            print("Up")
+            y = -2
         else:
-            print("Down")
-            y += 2
+            y = 2
     # jump left
     elif check == 3:
-        #print("Update check is")
         frame = jump_Left_Frames[cycle]
         cycle, event_number = gif_work(cycle, jump_Left_Frames, event_number, 1, 15)
-        x -= 2
+        x = -2
         if cycle < len(jump_Left_Frames) / 2:
-            print("Up")
-            y -= 3
+            y = -3
         else:
-            print("Down")
-            y += 3
+            y = 3
     # roll left
     elif check == 4:
         frame = roll_Left_Frames[cycle]
-        cycle, event_number = gif_work(cycle, roll_Left_Frames, event_number, 1,15)
-        x -= 3
+        cycle, event_number = gif_work(cycle, roll_Left_Frames, event_number, 1, 15)
+        x = -3
     # roll right
     elif check == 5:
         frame = roll_Right_Frames[cycle]
         cycle, event_number = gif_work(cycle, roll_Right_Frames, event_number, 1, 15)
-        x += 3
-    # 480x480 is the window size
-    # str(x) + 1050 is the position of the window
-    # we only updates x on roll left or roll right because it changes position
-    window.geometry('480x480+' + str(x) + '+' + str(y))
-    #print(window.geometry())
-    # 480 is sprite size, 1050 is supposed to be screen res
-    #print("PROCEEDING")
+        x = 3
+
+    window_details = window.geometry().split('+')
+    curr_x = int(window_details[1])
+    curr_y = int(window_details[2])
+    window.geometry('+{}+{}'.format(str(curr_x + x), str(curr_y + y)))
+
     label.configure(image=frame)
-    #print("SET")
+
     # after 1ms, perform event with event_number returned from 
-    #print(cycle, check, event_number, x)
-    window.after(1, event, cycle, check, event_number, x, y)
+
+    window.after(1, event, cycle, check, event_number)
 
 def debug_event(event_number):
     if event_number in idle:
@@ -161,8 +147,12 @@ roll_Right_Frames = [tk.PhotoImage(file=impath + 'rollRight.GIF', format='gif -i
 # window configuration
 label = tk.Label(window, bd=0, bg='white')
 label.pack()
-#window.geometry('480x480')
-#window.eval('tk::PlaceWindow . center')
+# place window at center of screen
+window.eval('tk::PlaceWindow . center')
+window_details = window.geometry().split('+')
+x = int(window_details[1]) - dimensions[0] // 2
+y = int(window_details[2]) - dimensions[1] // 2
+window.geometry('+{}+{}'.format(str(x), str(y)))
 # loop the program
-window.after(1, update, cycle, check, event_number, x, y)
+window.after(1, update, cycle, check, event_number)
 window.mainloop()
