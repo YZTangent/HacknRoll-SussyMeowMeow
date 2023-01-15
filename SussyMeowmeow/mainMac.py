6233a8a0b2e2329cd1599370bda2e6f16365c864
YZTangent <yuanzheng.tan.tyz@gmail.com>
2023-01-15 10:03:27 +0800

    ported changes to mac

--- SussyMeowmeow/mainMac.py
+++ SussyMeowmeow/mainMac.py
@@ -5,6 +5,7 @@ import tkinter as tk
 from PIL import Image, ImageTk
 import time
 from playsound import playsound
+from math import floor
 
 cat_dimensions = (120, 120)
 center_dimensions = None
@@ -117,38 +118,45 @@ def update(cycle, check, event_number, window, label):
     elif check == 1:
         frame = jump_Up_Frames[cycle]
         cycle, event_number = gif_work(cycle, jump_Up_Frames, event_number, 1, numEvents)
-        if cycle < len(jump_Up_Frames) / 2:
-            y = -6
-        else:
-            y = 2
+        # if cycle < len(jump_Up_Frames) / 2:
+        #     y = -20
+        # else:
+        #     y = 20
+        if cycle != 0:
+            y = floor(20 * (cycle - 3))
+        print("up " + str(cycle) + " " + str(y))
     # jump right
     elif check == 2:
         frame = jump_Right_Frames[cycle]
         cycle, event_number = gif_work(cycle, jump_Right_Frames, event_number, 1, numEvents)
-        x = 7
-        if cycle < len(jump_Right_Frames) / 2:
-            y = -5
-        else:
-            y = 5
+        x = 12
+        # if cycle < len(jump_Right_Frames) / 2:
+        #     y = -2
+        # else:
+        #     y = 2
+        y = floor(3 * (cycle - (len(jump_Right_Frames) / 2)))
+        # print("right " + str(cycle) + " " + str(y))
     # jump left
     elif check == 3:
         frame = jump_Left_Frames[cycle]
         cycle, event_number = gif_work(cycle, jump_Left_Frames, event_number, 1, numEvents)
-        x = -7
-        if cycle < len(jump_Left_Frames) / 2:
-            y = -5
-        else:
-            y = 5
+        x = -12
+        # if cycle < len(jump_Left_Frames) / 2:
+        #     y = -3
+        # else:
+        #     y = 3
+        y = floor(3 * (cycle - (len(jump_Left_Frames) / 2)))
+        # print("left " + str(cycle) + " " + str(y))
     # roll left
     elif check == 4:
         frame = roll_Left_Frames[cycle]
         cycle, event_number = gif_work(cycle, roll_Left_Frames, event_number, 1, numEvents)
-        x = -5
+        x = -10
     # roll right
     elif check == 5:
         frame = roll_Right_Frames[cycle]
         cycle, event_number = gif_work(cycle, roll_Right_Frames, event_number, 1, numEvents)
-        x = 5
+        x = 10
     # screm
     elif check == 6:
         frame = screm_Frames[cycle]
